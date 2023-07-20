d341a00974351383197105b4a868276e03953a83
Oleksii Sokolov <losasokolov@gmail.com>
2023-07-20 13:38:34 +0200

    + Account Item Card design + Account Main design

--- MysteryBoxProject/src/pages/account_page/Account.js
+++ MysteryBoxProject/src/pages/account_page/Account.js
@@ -1,11 +1,26 @@
 import React from "react";
 
+import AccountItemCard from "./AccountItemCard";
+
+import "./account.css";
+
+import jordan from "../../img/items_imgs/clothing/jordan1-dark.webp";
 
 const Account = () =>{
   return(
-    <div>
-
-    </div>
+    <main className="main" id="account-main">
+      <div className="account-container">
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+        <AccountItemCard img={jordan} itemName={"four"} itemPrice={10.99} />
+      </div>
+    </main>
   )
 }
 
---
+++ MysteryBoxProject/src/pages/account_page/AccountItemCard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./account-item-card.css";
+
+const AccountItemCard = (props) =>{
+  return(
+    <div className="account-item-card">
+      <div className="account-item-card__buttons">
+        <a href="/mimiammu" className="account-item-card-buttons__button" id="get-item-button">Withdraw</a>
+        <a href="/mimimamu" className="account-item-card-buttons__button" id="sell-item-button">Sell</a>
+      </div>
+      <img src={props.img} alt="" />
+      <div className="item-info">
+        <p1>{props.itemName}</p1>
+        <p2>{props.itemPrice} $</p2>
+      </div>
+    </div>
+  )
+}
+
+export default AccountItemCard;
